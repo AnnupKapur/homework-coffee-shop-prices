e09eafd9d95d6a507a92acc5518cebf8585b65c9
AnnupKapur <AnnupKapur@gmail.com>
2025-10-19 18:06:59 +0100

    init project - set homework task

---
+++ homework_task.py
@@ -0,0 +1,50 @@
+# Homework Task
+#
+# Create a cafe menu
+# Print out the menu (now with prices)
+#
+# Ask the user for which item they want
+# Ask the user how many of that item they want
+#
+# Ask the user if they want another item
+# Store their choices
+#
+# When they are finished
+# Print the list of items they ordered
+#
+#
+# e.g.
+# Welcome to MyCafe. What would you like:
+# - Coffee (£1.40)
+# - Cake (£2.00)
+# - Pain-Au-Chocolate (£1.80)
+# - Biscuit (£1.20)
+# - Tea (£1.00)
+# - Decaf(like a heathen) (£1.50)
+#
+# Make a choice: espresso
+# How many: 2   --- (as an int)
+# Would you like to make another choice: Yes
+# Make a choice: cookie
+# How many: 1   --- (as an int)
+# Would you like to make another choice: Yes
+# Make a choice: cake
+# How many: 3   --- (as an int)
+# Would you like to make another choice: No
+#
+# Here is your receipt:
+# - 2 x Espresso : £2.80
+# - 1 x Cookie : £1.20
+# - 3 x Cake : £6.00
+# Total: £10.00
+#
+# Print the menu and receipt in a fancy way
+# use f strings and string methods
+# e.g. print(f"{item} - {quantity}")
+# https://www.w3schools.com/python/python_strings_methods.asp
+#
+#
+# HINT: STORE MONEY AS PENCE (INT)
+#      £1.40 = 140 PENCE 
+#      THEN CONVERT TO POUNDS WHEN PRINTING
+#
