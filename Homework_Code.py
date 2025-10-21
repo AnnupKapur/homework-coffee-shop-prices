0d0bbc46bb805672de852e4060e676faa7bc3cae
AnnieKapur <annikakapur1996@gmail.com>
2025-10-21 08:52:26 +0100

    Steps 3 and 4 of Code

--- Homework_Code.py
+++ Homework_Code.py
@@ -1,14 +1,14 @@
 # Step 1: Create a café menu with prices
 menu = {
-    "Espresso": 2.00,
-    "Americano": 2.50,
-    "Latte": 3.00,
-    "Cappuccino": 3.20,
-    "Mocha": 3.50,
-    "Macchiato": 3.30,
-    "Flat White": 3.00,
-    "Hot Chocolate": 2.80,
-    "Tea": 1.80
+    "Espresso": 200,
+    "Americano": 250,
+    "Latte": 300,
+    "Cappuccino": 320,
+    "Mocha": 350,
+    "Macchiato": 330,
+    "Flat White": 300,
+    "Hot Chocolate": 280,
+    "Tea": 180
 }
 
 # Step 2: Print the menu
@@ -17,4 +17,16 @@ print("----------------------------")
 print("Drinks Menu:")
 
 for item, price in menu.items():
-    print(f"{item:15} £{price:.2f}")
+    print(f"{item:15} {price}p")
+
+# Step 3: Ask the user for which item they want
+# Step 4: Ask the user how many of that item they want
+
+
+choice = input("Which item would you like? ")
+
+if choice in menu:
+    quantity = int(input(f"How many {choice}s would you like? "))
+    total = menu[choice] * quantity
+else:
+    print("Sorry, that item isn't on the menu.")
