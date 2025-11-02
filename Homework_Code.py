7a1b780f6ab619626d38071a3bdfcc857b8aeaa9
AnnupKapur <AnnupKapur@gmail.com>
2025-11-02 11:42:09 +0000

    functional update

--- Homework_Code.py
+++ Homework_Code.py
@@ -8,13 +8,23 @@
 # change the menu to lower case in the function
 # 2: go through code and deconstruct more
 
+def user_input_match_drink(menu):
+    choice = input("🩷 Which item would you like?🩷 ").strip()
+    drink_name = match_input_to_menu_drink(menu, choice)
+    return drink_name
+
+def request_quantity_and_save(drink_name, order):
+    quantity = int(input(f"How many {drink_name}s would you like? ").strip())
+    new_order_item = {"item": drink_name, "quantity": quantity}
+    order.append(new_order_item)
+
 def pence_to_pound(pence):
     return f"£{pence / 100:.2f}"
 
 def calculate_total(menu, order):
     total_pence = 0
     for row in order:
-        total_pence = row["quantity"] * menu[row["item"]]
+        total_pence += row["quantity"] * menu[row["item"]]
     return total_pence
 
 # Step 1: Create a café menu with prices
@@ -29,13 +39,18 @@ menu = {
     "Hot Chocolate": 280,
     "Tea": 180,
 }
-def the_drink(menu, user_drink):
-    menu_lowercase = {item.lower(): item for item in menu}
+
+def match_input_to_menu_drink(menu, user_drink):
+    menu_lowercase = {}
+    for menu_item in menu:
+        lower_menu_item = menu_item.lower()
+        menu_lowercase[lower_menu_item] = menu_item
+
     user_drink_lowercase = user_drink.strip().lower()
     if user_drink_lowercase in menu_lowercase:
-        return True, menu_lowercase[user_drink_lowercase]
+        return menu_lowercase[user_drink_lowercase]
     else:
-        return False, None
+        return None
 
 
 # Step 2: Print the menu
@@ -53,14 +68,11 @@ order = []
 
 while True:
     another = "yes"
-    choice = input("🩷 Which item would you like?🩷 ").strip()
-    exists, drink_name = the_drink(menu, choice)    
-    if exists:
-        quantity = int(input(f"How many {drink_name}s would you like? ").strip())
 
-        new_order_item = {"item": drink_name, "quantity": quantity}
-        order.append(new_order_item)
+    drink_name = user_input_match_drink(menu)
 
+    if drink_name != None:
+        request_quantity_and_save(drink_name, order)
         another = input("🩷 Would you like to order another item? (yes/no): ").strip().lower()
         if another != "yes":
             break
